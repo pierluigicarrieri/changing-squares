968d378b588ece2d1fd893a0a412b91be87fb8ee
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-12-02 01:35:31 +0100

    Inner html hidden

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 
 const square = [];
 
-const squaresNumber = 225;
+const squaresNumber = 400;
 
 // const difficultySelectorElement = document.getElementById("difficulty_selector");
 // const minefieldCreatorElement = document.getElementById("minefield_creator");
